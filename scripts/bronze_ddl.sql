43ca2218570532ec491b3ad02c3461c80c5307a1
Stevenomonaa <stevenomonaa@gmail.com>
2026-03-02 11:20:06 +0100

    Add header comment to bronze DDL script
    Added header comment to describe the purpose of the DDL script.

--- scripts/bronze_ddl.sql
+++ scripts/bronze_ddl.sql
@@ -1,6 +1,14 @@
-bronze DDL
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
-CREATE SCHEMA bronze;
 
 -- CRM Customers
 DROP TABLE IF EXISTS bronze.crm_customers;
